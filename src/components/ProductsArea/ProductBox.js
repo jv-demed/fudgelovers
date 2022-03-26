b7e4135ccf0a97949fb0de41c2cd11ff5d3e763a
jv-medeiros <jv.demed@gmail.com>
2022-03-26 01:06:14 -0300

    fl v0.1.2

--- src/components/ProductsArea/ProductBox.js
+++ src/components/ProductsArea/ProductBox.js
@@ -5,26 +5,42 @@ const Box = styled.div`
     background-color: rgba(0,0,0,0.3);
     border-radius: 5px;
     display: flex;
+    height: 200px;
     img{
         border-radius: 5px 0 0 5px;
-        height: 200px;
+        height: 100%;
         min-width: 350px;
     }
     .content{
         display: flex;
         flex-direction: column;
         justify-content: space-between;
-        padding: 0 20px 15px;
+        padding: 5px 0 15px 20px;
         width: 100%;
+        .text{
+            overflow: auto;
+            padding-right: 20px;
+            ::-webkit-scrollbar{
+                width: 4px;
+            }
+            ::-webkit-scrollbar-thumb{
+                background: ${({ theme }) => theme.palette.white};
+            }
+        }
         .btnBox{
             display: flex;
             justify-content: right;
+            margin-right: 20px;
             margin-top: 15px;
         }
     }
+    @media(max-width: 850px){
+        img{
+            min-width: 280px;
+        }
+    }
     @media(max-width: 650px){
-        align-items: center;
-        flex-direction: column;
+        height: auto;
         img{
             display: none;
         }
